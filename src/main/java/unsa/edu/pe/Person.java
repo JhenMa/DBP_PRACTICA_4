1ee9c9f15fe34605d38e446a7c0462208bae601f
JhenMa <71655718+JhenMa@users.noreply.github.com>
2021-05-25 18:42:05 -0500

    Add files via upload

---
+++ src/main/java/unsa/edu/pe/Person.java
@@ -0,0 +1,26 @@
+package unsa.edu.pe;
+
+public class Person {
+	private String name;
+	private Integer age;
+	
+	
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+}
